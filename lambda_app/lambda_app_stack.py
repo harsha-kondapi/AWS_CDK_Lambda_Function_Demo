bf495913cdcf79a911d42dc8cd064581fbccdf49
harsha-kondapi <harshakondapi11@gmail.com>
2023-04-14 20:05:34 +0530

    first commit

--- lambda_app/lambda_app_stack.py
+++ lambda_app/lambda_app_stack.py
@@ -1,6 +1,9 @@
 from aws_cdk import (
     # Duration,
     Stack,
+    aws_lambda as _lambda,
+    aws_apigateway as apigw
+    aws_s3 as s3
     # aws_sqs as sqs,
 )
 from constructs import Construct
@@ -17,3 +20,14 @@ class LambdaAppStack(Stack):
         #     self, "LambdaAppQueue",
         #     visibility_timeout=Duration.seconds(300),
         # )
+        hello_function = _lambda.Function(self, "WelcomeHandler",
+                              runtime=_lambda.Runtime.PYTHON_3_8,
+                              handler="welcome.handler",
+                          #   code=_lambda.Code.from_asset(path.join(__dirname, 'lambda-api'))
+                              code=_lambda.Code.from_asset('lambda-api')
+                              )
+        apigw.LambdaRestApi(
+            self,'Endpoint',
+            handler = hello_function
+            )
+
